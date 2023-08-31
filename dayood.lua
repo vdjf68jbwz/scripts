15d47ac3ee6b6e86f067b58baaf0078df466cf50
Zach <99155118+NooobLorod@users.noreply.github.com>
2023-08-31 19:49:34 -0400

    Update dayood.lua

--- dayood.lua
+++ dayood.lua
@@ -1,5 +1,5 @@
 local script1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Allvideo/test/main/Whitelist.txt"))()
-local script2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/NooobLorod/loadstrings/77b545213a3d5b5767767631f35162b48386866b/eclipse.lua"))()
+local script2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/NooobLorod/loadstrings/main/skidded%20dayood%20lock.lua"))()
 
 local function executeScript(script)
     local thread = coroutine.create(function()
