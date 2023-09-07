afa1e5ddc4815a3c2b1408a603a5d4041dc958ad
Zach <99155118+NooobLorod@users.noreply.github.com>
2023-09-06 20:47:42 -0400

    Update dayood.lua

--- dayood.lua
+++ dayood.lua
@@ -1,5 +1,6 @@
 local script1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Allvideo/test/main/Whitelist.txt"))()
 local script2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/NooobLorod/loadstrings/main/no%20prem%20commands%20swagmode.lua"))()
+local script3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/NooobLorod/loadstrings/main/landuage%20bypass.lua"))()
 
 local function executeScript(script)
     local thread = coroutine.create(function()
@@ -10,3 +11,4 @@ end
 
 executeScript(script1)
 executeScript(script2)
+executeScript(script3)
