4546cbef2eb3b82d768a45f9fd2146b1c97695af
vdjf68jbwz <99155118+vdjf68jbwz@users.noreply.github.com>
2025-02-23 12:01:21 -0500

    Create all.lua

---
+++ all.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 9825515356 then 
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
+  local set = 0.04654492465
+  setclipboard(tostring(set))
+elseif game.PlaceId == 292439477 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
+elseif game.PlaceId == 5289429734 then 
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/vdjf68jbwz/scripts/refs/heads/main/blackhawkesp.lua"))()
+elseif game.PlaceId == 4581966615 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+elseif game.PlaceId == 258258996 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/vdjf68jbwz/scripts/refs/heads/main/minerhaven.lua"))()
+end
